cccaefa1ba3043fffd82d2613321cd3d8a377d84
Matt Eads <matteads@Matts-MacBook-Pro.local>
2020-04-05 18:14:40 -0400

    Updated .git ignore and conda env

--- setup.py
+++ setup.py
@@ -1,5 +1,36 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-'''
+setup(
+    name="AWS-Tutorials",
+    version="1.0.0",
+    packages=find_packages(),
+    scripts=["app.py"],
 
-'''
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires=["docutils>=0.3"],
+
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        "": ["*.txt", "*.rst"],
+        # And include any *.msg files found in the "hello" package, too:
+        "hello": ["*.msg"],
+    },
+
+    # metadata to display on PyPI
+    author="Matt Eads",
+    author_email="matteads2@gmail.com",
+    description="This is a simple pacakge to learn AWS core services for your next project",
+    keywords="AWS Cloud Docker Tutorial Python Boto3",
+    #url="http://example.com/HelloWorld/",   # project home page, if any
+    project_urls={
+        "Bug Tracker": "https://bugs.example.com/HelloWorld/",
+        "Documentation": "https://docs.example.com/HelloWorld/",
+        "Source Code": "https://code.example.com/HelloWorld/",
+    },
+    classifiers=[
+        "License :: OSI Approved :: Python Software Foundation License"
+    ]
+
+    # could also include long_description, download_url, etc.
+)
---
+++ tests/test_condaenv.py
@@ -0,0 +1,3 @@
+'''
+Placeholder for conda env setup and tests
+'''
